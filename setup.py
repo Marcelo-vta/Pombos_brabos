b8cc9de8c677474038a84b2c73672981dc9bb3e5
henriquebrnetto <henriquebrnetto02@gmail.com>
2024-08-26 23:51:42 -0300

    tentativa 6 pip install

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="Pombos_brabos",  # Nome do seu projeto
-    version="0.1.0",  # Versão inicial do seu projeto
-    packages=find_packages(),  # Pacotes a serem incluídos na distribuição
+    version="0.1.0",
     author="Marcelo e Henrique",  # Nome do autor do projeto
     author_email="mclonso123@outlook.com",  # Email do autor
     description="Projeto de alglin",  # Descrição do projeto
-    long_description=open("README.md").read(),  # Descrição longa do projeto (tipicamente do README.md)
+    long_description=long_description,
     long_description_content_type="text/markdown",  # Tipo de conteúdo da descrição longa
     url="https://github.com/Marcelo-vta/Pombos_brabos",  # URL do repositório do projeto
+    packages=find_packages(),  # Pacotes a serem incluídos na distribuição
     classifiers=[  # Classificadores para facilitar a busca do projeto
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11',  # Versão mínima do Python,
+    python_requires='>=3.11',
     entry_points={
         'console_scripts': [
-            'marcelo_Pombos_brabos=Pombos_brabos.main:main',
+            'tiago_hello_world=hello_world.main:main',
         ],
     },
+
     install_requires=[  # Instala as dependências especificadas no requirements.txt
         line.strip() for line in open("requirements.txt").readlines()
-    ]
+    ],
 )
